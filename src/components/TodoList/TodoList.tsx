1a4c53b51891eaf3f87daeae218717c96b2f78e0
feliciacalming <felicia.calming@gmail.com>
2023-05-16 19:21:27 +0200

    (feat): add TodoList component

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,18 @@
+import { Todo } from "../../models/Todo";
+import { ShowTodo } from "../ShowTodo/ShowTodo";
+
+interface TodoListProps {
+  todos: Todo[];
+}
+
+export const TodoList = ({ todos }: TodoListProps) => {
+  return (
+    <>
+      <div className="todos-container">
+        {todos.map((todo, index) => {
+          return <ShowTodo todo={todo}></ShowTodo>;
+        })}
+      </div>
+    </>
+  );
+};
