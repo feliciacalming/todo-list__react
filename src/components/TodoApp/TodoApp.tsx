f9d653c862ca37184161dd2b95dea987665a8387
feliciacalming <felicia.calming@gmail.com>
2023-05-16 15:29:28 +0200

    (feat): create add function in TodoApp

--- src/components/TodoApp/TodoApp.tsx
+++ src/components/TodoApp/TodoApp.tsx
@@ -1,3 +1,22 @@
+import { useReducer } from "react";
+import { ActionType, TodosReducer } from "../../reducers/TodosReducer";
+import { AddTodo } from "../AddTodo/AddTodo";
+import { ShowTodo } from "../ShowTodo/ShowTodo";
+
 export const TodoApp = () => {
-  return <></>;
+  const [todos, dispatch] = useReducer(TodosReducer, []);
+
+  const add = (text: string) => {
+    console.log("add i TodoApp körs");
+    dispatch({ type: ActionType.ADD, payload: text });
+  };
+
+  console.log("Todos in app", todos);
+
+  return (
+    <>
+      <AddTodo createTodo={add}></AddTodo>
+      <ShowTodo todos={todos}></ShowTodo>
+    </>
+  );
 };
