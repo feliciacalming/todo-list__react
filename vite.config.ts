9a5a8b2c5d7c69c1ef78544d8e1df9e0c3ad366c
feliciacalming <felicia.calming@gmail.com>
2023-05-18 09:55:16 +0200

    (feat): add scss-mixins and variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "./src/styles/variables" as var; @use "./src/styles/mixins" as mix;`,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
