e394bf019fff5771333f1ab30b29b9e6a43ec40a
feliciacalming <felicia.calming@gmail.com>
2023-05-19 11:15:01 +0200

    (feat): add contexts in TodoApp

--- src/components/TodoApp/TodoApp.tsx
+++ src/components/TodoApp/TodoApp.tsx
@@ -1,40 +1,40 @@
 import "./TodoApp.scss";
-import { useEffect, useReducer } from "react";
+import { useReducer } from "react";
 import { ActionType, TodosReducer } from "../../reducers/TodosReducer";
 import { AddTodo } from "../AddTodo/AddTodo";
 import { TodoList } from "../TodoList/TodoList";
-import { saveToLS } from "../../helpers/saveToLS";
+import { TodosContext } from "../../contexts/TodosContext";
+import { TodosDispatchContext } from "../../contexts/TodosDispatchContext";
 
 export const TodoApp = () => {
   const [todos, dispatch] = useReducer(TodosReducer, []);
 
-  useEffect(() => {
-    saveToLS(todos);
-  });
-
-  const add = (text: string) => {
-    dispatch({ type: ActionType.ADD, payload: text });
-    saveToLS(todos);
-  };
+  // useEffect(() => {
+  //   saveToLS(todos);
+  // }, [todos]);
 
   const toggle = (id: number) => {
-    dispatch({ type: ActionType.TOGGLE, payload: id });
+    dispatch({ type: ActionType.TOGGLED, payload: id.toString() });
   };
 
   const deleteTodo = (id: number) => {
-    dispatch({ type: ActionType.DELETE, payload: id });
+    dispatch({ type: ActionType.DELETED, payload: id.toString() });
   };
 
   return (
     <>
       <div className="wrapper">
         <h1 className="title">Things to do today:</h1>
-        <AddTodo createTodo={add}></AddTodo>
-        <TodoList
-          toggle={toggle}
-          todos={todos}
-          deleteTodo={deleteTodo}
-        ></TodoList>
+        <TodosContext.Provider value={todos}>
+          <TodosDispatchContext.Provider value={dispatch}>
+            <AddTodo></AddTodo>
+            <TodoList
+              toggle={toggle}
+              todos={todos}
+              deleteTodo={deleteTodo}
+            ></TodoList>
+          </TodosDispatchContext.Provider>
+        </TodosContext.Provider>
       </div>
     </>
   );
