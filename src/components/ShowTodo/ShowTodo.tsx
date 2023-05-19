da5cab911cfa6e1d65ff9be86c4080831a325c33
feliciacalming <felicia.calming@gmail.com>
2023-05-19 13:18:13 +0200

    (refactor): useContext instead of functions in props

--- src/components/ShowTodo/ShowTodo.tsx
+++ src/components/ShowTodo/ShowTodo.tsx
@@ -1,31 +1,43 @@
 import "./ShowTodo.scss";
+import { useContext } from "react";
+import { TodosDispatchContext } from "../../contexts/TodosDispatchContext";
+import { ActionType } from "../../reducers/TodosReducer";
 import { Todo } from "../../models/Todo";
 
-interface ShowTodoProps {
+interface IShowTodoProps {
   todo: Todo;
-  toggleTodo: (id: number) => void;
-  deleteTodo: (id: number) => void;
 }
 
-export const ShowTodo = ({ todo, toggleTodo, deleteTodo }: ShowTodoProps) => {
-  const handleCheckbox = () => {
-    toggleTodo(todo.id);
-  };
+export const ShowTodo = ({ todo }: IShowTodoProps) => {
+  const dispatch = useContext(TodosDispatchContext);
 
   return (
     <>
       <div className="todo">
-        <div className="todo__task-container">
+        <div
+          className="todo__task-container"
+          onClick={() =>
+            dispatch({
+              type: ActionType.TOGGLED,
+              payload: todo.id.toString(),
+            })
+          }
+        >
           <input
             type="checkbox"
             className="todo__checkbox"
-            onChange={handleCheckbox}
+            checked={todo.done}
           />
           <li className={`todo__task ${todo.done ? "done" : ""}`}>
             {todo.task}
           </li>
         </div>
-        <button className="todo__deleteBtn" onClick={() => deleteTodo(todo.id)}>
+        <button
+          className="todo__deleteBtn"
+          onClick={() => {
+            dispatch({ type: ActionType.DELETED, payload: todo.id.toString() });
+          }}
+        >
           <i className="bi bi-x-square"></i>
         </button>
       </div>
