bcf898e2e5e99204cc00d9e517a07c45ef116855
feliciacalming <felicia.calming@gmail.com>
2023-05-19 11:02:47 +0200

    (refactor): change action types

--- src/reducers/TodosReducer.ts
+++ src/reducers/TodosReducer.ts
@@ -1,33 +1,34 @@
 import { Todo } from "../models/Todo";
 
-interface IAction {
+export interface IAction {
   type: ActionType;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  payload: any;
+  payload: string;
 }
 
 export enum ActionType {
-  ADD,
-  TOGGLE,
-  DELETE,
+  ADDED,
+  TOGGLED,
+  DELETED,
 }
 
 export const TodosReducer = (todos: Todo[], action: IAction) => {
   switch (action.type) {
-    case ActionType.ADD: {
+    case ActionType.ADDED: {
       return [...todos, new Todo(action.payload, false, new Date().getTime())];
     }
 
-    case ActionType.TOGGLE: {
+    case ActionType.TOGGLED: {
       return todos.map((todo) => {
-        if (todo.id === action.payload) {
+        console.log(action.payload);
+        if (todo.id.toString() === action.payload) {
           return { ...todo, done: !todo.done };
         }
         return todo;
       });
     }
 
-    case ActionType.DELETE: {
+    case ActionType.DELETED: {
       return todos.filter((todo) => todo.id !== action.payload);
     }
 
